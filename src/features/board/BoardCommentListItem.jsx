081e57b907ef8e0e5bf6e7b45cd962cab4459ba4
Mabin <mabinum9528@naver.com>
2024-07-03 16:58:11 +0900

    게시판 댓글 수정기능

---
+++ src/features/board/BoardCommentListItem.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import styled from "styled-components";
+import { BsPencilSquare } from "react-icons/bs";
+import { CiSquareRemove } from "react-icons/ci";
+import { useDispatch } from "react-redux";
+import { closeEditComment } from "./boardSlice";
+
+const DivContainer = styled.div`
+  width: 50%;
+  display: flex;
+  font-size: 20px;
+  justify-content: space-between;
+`;
+function BoardCommentListItem(props) {
+  const {content,index,isEdit,commentNo,writer,setCommentList} = props;
+
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState(content);
+
+  const handleModifyContentOpen = () => {
+    setCommentList(commentList => {
+      return commentList.map(comment => comment.commentNo === commentNo 
+        ? { ...comment, isEdit: true }
+        : { ...comment, isEdit: false }
+      );
+    });
+  };
+
+  const handleModifyContentClose = () => {
+    setShowEdit(false);
+  };
+
+  const handleEditChange = (e) => {
+    dispatch()
+    setValue(e.target.value);
+  }
+
+  const handleRemoveComment = () => {
+  };
+  
+  // const modifyComment = async() => {
+  //   try{
+  //     const response = await axios.put(`http://localhost:8080/comment/modify`, {
+  //       "commentNo" : 0,
+  //       "content" : comment
+  //     },
+  //     {
+  //       headers : {
+  //         Authorization : localStorage.getItem('token')
+  //       }
+  //     });
+  //     if (response.status === 200) { 
+  //       return alert("댓글이 수정되었습니다.");
+  //     } else { 
+  //       throw new Error(`a  pi error: ${response.status} ${response.statusText}`);
+  //     }
+  //   } catch(err) {
+  //     console.error(err);
+  //   }
+  // };
+
+
+  return (
+    <DivContainer key={commentNo}>
+      {
+        isEdit ? 
+        <InputGroup className="mb-3">
+          <Form.Control value={value}  onChange={handleEditChange}/>
+          <Button variant="outline-warning" onClick={handleModifyContentClose}>수정 확인</Button>
+        </InputGroup> :
+        <div key={index}>{writer} : {content}</div>
+      }
+        <div>
+          <BsPencilSquare onClick={handleModifyContentOpen}/>
+          <CiSquareRemove onClick={handleRemoveComment}/>
+        </div>
+    </DivContainer>
+  );
+};
+
+export default BoardCommentListItem;
